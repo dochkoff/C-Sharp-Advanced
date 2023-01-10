1461d0f0ae8d9f3ce28534e8bfb0f4b0cbeeef05
dochkoff <hounds.07cinder@icloud.com>
2023-01-10 23:32:50 +0200

    T03-P01

---
+++ C# Advanced - 2023/T03_StacksAndQueues/P01_ReverseAString/P01_ReverseAString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var revercedString = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                revercedString.Push(input[i]);
+            }
+
+            while (revercedString.Count != 0)
+            {
+                Console.Write(revercedString.Pop());
+            }
+        }
+    }
+}
