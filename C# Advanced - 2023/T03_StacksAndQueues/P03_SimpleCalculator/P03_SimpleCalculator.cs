99d1f296d63c2fa3cb89a04c6b3388595ab5d46e
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-12 13:00:35 +0200

    T03-P03

---
+++ C# Advanced - 2023/T03_StacksAndQueues/P03_SimpleCalculator/P03_SimpleCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03_SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+
+            var expression = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                expression.Push(input[i]);
+            }
+
+            int result = int.Parse(expression.Pop());
+
+            while (expression.Count > 0)
+            {
+                char sign = char.Parse(expression.Pop());
+
+                if (sign == '+')
+                {
+                    result += int.Parse(expression.Pop());
+                }
+                else
+                {
+                    result -= int.Parse(expression.Pop());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
