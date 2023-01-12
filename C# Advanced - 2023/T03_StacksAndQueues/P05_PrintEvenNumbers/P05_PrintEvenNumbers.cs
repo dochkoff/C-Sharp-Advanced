88589856ccd51efe55aff6c0426ad73d0e3531fa
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-12 16:46:52 +0200

    T03-P05

---
+++ C# Advanced - 2023/T03_StacksAndQueues/P05_PrintEvenNumbers/P05_PrintEvenNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05_PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var numsQueu = new Queue<int>(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numsQueu.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    Console.Write(number);
+
+                    if (numsQueu.Count > 0)
+                    {
+                        Console.Write($", ");
+                    }
+                }
+            }
+        }
+    }
+}
