2deeff902ab7cd38952117e89ef35b4fb2644611
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-12 18:05:53 +0200

    T03-P06

---
+++ C# Advanced - 2023/T03_StacksAndQueues/P06_Supermarket/P06_Supermarket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new Queue<string>();
+
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    names.Enqueue(input);
+                }
+                else if (input == "Paid")
+                {
+                    for (int i = 0; i < names.Count(); i++)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count()} people remaining.");
+        }
+    }
+}
