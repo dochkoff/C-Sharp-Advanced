280a1f273f9dad1d612f993a1d019b5464fb1a58
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-12 13:26:01 +0200

    T03-P04

---
+++ C# Advanced - 2023/T03_StacksAndQueues/P04_MatchingBrackets/P04_MatchingBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04_MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var openBracketIndex = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    openBracketIndex.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+
+                    int openBracket = openBracketIndex.Pop();
+
+                    for (int j = openBracket; j <= i; j++)
+                    {
+                        Console.Write(input[j]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
