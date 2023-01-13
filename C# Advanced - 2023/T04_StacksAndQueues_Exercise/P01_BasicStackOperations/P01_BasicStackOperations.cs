bd3ff74b5eb4cd05ef8400c4565b94c0dc5949e4
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-13 23:59:54 +0200

    T04-P01

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P01_BasicStackOperations/P01_BasicStackOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int N = parameters[0]; //number of elements to push into the stack
+            int S = parameters[1]; //number of elements to pop from the stack
+            int X = parameters[2]; //an element that you should look for in the stack
+
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            for (int i = 0; i < S; i++)
+            {
+                stack.Pop();
+            }
+
+
+            if (stack.Any())
+            {
+                if (stack.Contains(X))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
+
