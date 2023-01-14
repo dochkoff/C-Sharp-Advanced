01ff22413738f76ccc2934d9d49b58f580245356
dochkoff <hounds.07cinder@icloud.com>
2023-01-15 00:55:49 +0200

    T04-P09

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P09_SimpleTextEditor/P09_SimpleTextEditor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P09_SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+            Stack<string> undoChanges = new Stack<string>();
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split();
+
+                int commandIndex = int.Parse(command[0]);
+
+                if (commandIndex == 1)
+                {
+                    undoChanges.Push(text);
+                    text = text + command[1];
+                }
+                else if (commandIndex == 2)
+                {
+                    undoChanges.Push(text);
+                    int erasedElementsCount = int.Parse(command[1]);
+                    text = text.Remove(text.Length - erasedElementsCount);
+                }
+                else if (commandIndex == 3)
+                {
+                    int indexOfElement = int.Parse(command[1]) - 1;
+                    Console.WriteLine(text.ElementAt(indexOfElement));
+                }
+                else if (commandIndex == 4)
+                {
+                    text = undoChanges.Pop();
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
