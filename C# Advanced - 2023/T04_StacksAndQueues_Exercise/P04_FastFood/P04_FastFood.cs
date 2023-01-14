9251b26e0407860f06266134538687dcf3040e76
dochkoff <hounds.07cinder@icloud.com>
2023-01-14 13:00:21 +0200

    T04-P04

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P04_FastFood/P04_FastFood.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            int biggestOrder = ordersQueue.Max();
+            Console.WriteLine(biggestOrder);
+
+            while (ordersQueue.Any())
+            {
+                quantityOfFood -= ordersQueue.Peek();
+
+                if (quantityOfFood < 0)
+                {
+                    break;
+                }
+
+                ordersQueue.Dequeue();
+
+            }
+
+            if (quantityOfFood >= 0 && ordersQueue.Count() == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+            }
+        }
+    }
+}
