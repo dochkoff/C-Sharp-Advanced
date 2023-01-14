f530c149b8b2f3b7e6fe60a7f00a5ee2bbd97637
dochkoff <hounds.07cinder@icloud.com>
2023-01-14 18:02:57 +0200

    T04-P06

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P06_SongsQueue/P06_SongsQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace P06_SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songsQueue = new Queue<string>
+                (Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+
+
+            while (songsQueue.Any())
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (command[0] == "Add")
+                {
+                    string NewSongName = string.Join(" ", command).Substring(4);
+
+                    if (songsQueue.Contains(NewSongName))
+                    {
+                        Console.WriteLine($"{NewSongName} is already contained!");
+                    }
+                    else
+                    {
+                        songsQueue.Enqueue(NewSongName);
+                    }
+                }
+                else if (command[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
