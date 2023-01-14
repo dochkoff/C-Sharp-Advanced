e315e25d2efad5625622c153b37d2c681f8af3e2
dochkoff <hounds.07cinder@icloud.com>
2023-01-14 19:11:55 +0200

    T04-P07

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P07_TruckTour/P07_TruckTour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                int[] petrolPumpParams = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+                int litresPetrol = petrolPumpParams[0];
+                int nextPetrolPumpDistance = petrolPumpParams[1];
+
+                if (litresPetrol >= nextPetrolPumpDistance)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
