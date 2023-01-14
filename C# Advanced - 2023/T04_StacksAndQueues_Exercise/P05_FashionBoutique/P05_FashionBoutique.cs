21f43d1fff23e569d387c0ef11252d4b2eb9511b
dochkoff <hounds.07cinder@icloud.com>
2023-01-14 17:29:08 +0200

    T04-P05

---
+++ C# Advanced - 2023/T04_StacksAndQueues_Exercise/P05_FashionBoutique/P05_FashionBoutique.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesStack = new Stack<int>
+                (Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int currentRackCapacity = rackCapacity;
+            int rackCounter = 1;
+
+            while (clothesStack.Any())
+            {
+                currentRackCapacity -= clothesStack.Peek();
+
+                if (currentRackCapacity < 0)
+                {
+                    currentRackCapacity = rackCapacity;
+                    rackCounter++;
+
+                    continue;
+                }
+
+                clothesStack.Pop();
+            }
+
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
