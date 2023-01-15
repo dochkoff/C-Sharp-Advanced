54bd224eba8910dafe6279409a32193289bbc96f
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-15 23:16:11 +0200

    T05-P01

---
+++ C# Advanced - 2023/T05_Multidimensional_Arrays/P01_SumMatrixElements/P01_SumMatrixElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int sum = 0;
+
+            foreach (int element in matrix)
+            {
+                sum += element;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
+
