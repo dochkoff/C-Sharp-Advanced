2b32ae9f11d6ebfa34bf6b01133206df87413238
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-16 23:33:24 +0200

    T05-P07

---
+++ C# Advanced - 2023/T05_Multidimensional_Arrays/P07_PascalTriangle/P07_PascalTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P07_PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascal = new long[rows][];
+            pascal[0] = new long[1] { 1 };
+
+            for (int row = 1; row < pascal.Length; row++)
+            {
+                pascal[row] = new long[row + 1];
+
+                for (int col = 0; col < pascal[row].Length; col++)
+                {
+                    if (pascal[row - 1].Length > col)
+                    {
+                        pascal[row][col] += pascal[row - 1][col];
+                    }
+
+                    if (col > 0)
+                    {
+                        pascal[row][col] += pascal[row - 1][col - 1];
+                    }
+                }
+            }
+
+            foreach (long[] array in pascal)
+            {
+                Console.WriteLine(string.Join(" ", array));
+            }
+        }
+    }
+}
