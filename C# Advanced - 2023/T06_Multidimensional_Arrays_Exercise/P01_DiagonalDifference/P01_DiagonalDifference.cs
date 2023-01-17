3f7f6bed6f67bd9c2ba0af8af14f8b549de33f90
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-17 18:32:29 +0200

    T06-P01

---
+++ C# Advanced - 2023/T06_Multidimensional_Arrays_Exercise/P01_DiagonalDifference/P01_DiagonalDifference.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int leftDiagonal = 0;
+            int righttDiagonal = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                leftDiagonal += matrix[row, row];
+                righttDiagonal += matrix[row, size - 1 - row];
+            }
+
+            Console.WriteLine(Math.Abs(leftDiagonal - righttDiagonal));
+        }
+    }
+}
+
