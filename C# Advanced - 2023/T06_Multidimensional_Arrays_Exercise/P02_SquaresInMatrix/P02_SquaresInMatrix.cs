f7f004c8d546612825f3044f0b5eb314574aa405
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-17 21:33:13 +0200

    T06-P02

---
+++ C# Advanced - 2023/T06_Multidimensional_Arrays_Exercise/P02_SquaresInMatrix/P02_SquaresInMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dimensions = (Console.ReadLine());
+
+            int[] parameters = dimensions
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            char[,] matrix = new char[parameters[0], parameters[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] colElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int squareMatrixesCounter = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        squareMatrixesCounter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squareMatrixesCounter);
+        }
+    }
+}
+
