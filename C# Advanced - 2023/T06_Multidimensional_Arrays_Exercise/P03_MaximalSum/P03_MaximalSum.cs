4cd6709d03bba89e93262b458760b13de6de7afc
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-17 21:47:01 +0200

    T06-P03

---
+++ C# Advanced - 2023/T06_Multidimensional_Arrays_Exercise/P03_MaximalSum/P03_MaximalSum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int maxSum = 0;
+            int maxStartRow = 0;
+            int maxStartCol = 0;
+
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxStartRow = row;
+                        maxStartCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = maxStartRow; row <= maxStartRow + 2; row++)
+            {
+                for (int col = maxStartCol; col <= maxStartCol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
