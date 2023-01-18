368baa9c2200bcd6f5d6c345649d928e32596b61
dochkoff <hounds.07cinder@icloud.com>
2023-01-18 21:40:00 +0200

    T06-P10

---
+++ C# Advanced - 2023/T06_Multidimensional_Arrays_Exercise/T10_RadioactiveMutantVampireBunnies/T10_RadioactiveMutantVampireBunnies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T10_RadioactiveMutantVampireBunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
