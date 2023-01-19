d364edae7309e7c946eec37a68d8b9b15e080f32
dochkoff <hounds.07cinder@icloud.com>
2023-01-19 21:43:35 +0200

    T07-P03

---
+++ C# Advanced - 2023/T07_SetsAndDictionariesAdvanced/P03_Largest3Numbers/P03_Largest3Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+
+            if (sorted.Length > 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+        }
+    }
+}
