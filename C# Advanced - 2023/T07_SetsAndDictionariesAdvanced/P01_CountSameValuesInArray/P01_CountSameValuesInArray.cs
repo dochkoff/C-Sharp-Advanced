682fea533644a83c713ebee1747ab410bb23a862
dochkoff <hounds.07cinder@icloud.com>
2023-01-19 21:07:26 +0200

    T07-P01

---
+++ C# Advanced - 2023/T07_SetsAndDictionariesAdvanced/P01_CountSameValuesInArray/P01_CountSameValuesInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> count = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!count.Keys.Contains(input[i]))
+                {
+                    count.Add(input[i], 0);
+                }
+
+                count[input[i]]++;
+            }
+
+            foreach (var kvp in count)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
