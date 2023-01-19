6499438615f5c2edb3df231d478798508bdb4231
dochkoff <hounds.07cinder@icloud.com>
2023-01-19 21:21:40 +0200

    T07-P02

---
+++ C# Advanced - 2023/T07_SetsAndDictionariesAdvanced/P02_AverageStudentGrades/P02_AverageStudentGrades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entries = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentsGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < entries; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                decimal grade = decimal.Parse(info[1]);
+
+                if (!studentsGrades.ContainsKey(name))
+                {
+                    studentsGrades.Add(name, new List<decimal>());
+                }
+
+                studentsGrades[name].Add(grade);
+            }
+
+            foreach (var kvp in studentsGrades)
+            {
+                Console.Write(kvp.Key + " -> ");
+
+                foreach (var item in kvp.Value)
+                {
+                    Console.Write($"{item:F2} ");
+                }
+
+                Console.WriteLine($"(avg: {kvp.Value.Average():F2})");
+
+            }
+        }
+    }
+}
