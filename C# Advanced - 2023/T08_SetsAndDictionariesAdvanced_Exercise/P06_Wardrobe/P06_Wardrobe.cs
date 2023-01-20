e53781730fad3552384e8118b29fe40cc444cb94
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 22:59:53 +0200

    T08-P06

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P06_Wardrobe/P06_Wardrobe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardbore = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] colorAndClothing = Console.ReadLine().Split(" -> ");
+                string color = colorAndClothing[0];
+                string[] clothing = colorAndClothing[1].Split(",");
+
+                if (!wardbore.ContainsKey(color))
+                {
+                    wardbore.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int j = 0; j < clothing.Length; j++)
+                {
+                    if (!wardbore[color].ContainsKey(clothing[j]))
+                    {
+                        wardbore[color].Add(clothing[j], 0);
+                    }
+
+                    wardbore[color][clothing[j]]++;
+                }
+            }
+
+            string[] search = Console.ReadLine().Split();
+            string searchedColor = search[0];
+            string searchedClothes = search[1];
+
+            foreach (var kvp in wardbore)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+
+                foreach (var clothes in kvp.Value)
+                {
+                    if (kvp.Key == searchedColor && clothes.Key == searchedClothes)
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
