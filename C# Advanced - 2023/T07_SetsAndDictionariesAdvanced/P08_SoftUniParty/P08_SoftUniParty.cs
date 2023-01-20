fc602d7598902c67a44acb6b36a39435da898f2e
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 14:02:53 +0200

    T07-P08

---
+++ C# Advanced - 2023/T07_SetsAndDictionariesAdvanced/P08_SoftUniParty/P08_SoftUniParty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08_SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> reservationsVip = new HashSet<string>();
+            HashSet<string> reservationsRegular = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END" && input != "PARTY")
+            {
+                char firstChar = input[0];
+
+                if (char.IsDigit(firstChar))
+                {
+                    reservationsVip.Add(input);
+                }
+                else
+                {
+                    reservationsRegular.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            while (input != "END")
+            {
+                if (reservationsVip.Contains(input))
+                {
+                    reservationsVip.Remove(input);
+                }
+
+                if (reservationsRegular.Contains(input))
+                {
+                    reservationsRegular.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(reservationsVip.Count + reservationsRegular.Count);
+
+            foreach (var item in reservationsVip)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in reservationsRegular)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
