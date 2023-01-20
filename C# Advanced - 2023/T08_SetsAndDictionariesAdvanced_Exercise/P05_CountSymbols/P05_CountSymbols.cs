4acce4c31ef3ad92385ea1fcbac0efa59e568c97
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 20:47:35 +0200

    T08-P05

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P05_CountSymbols/P05_CountSymbols.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05_CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols.Add(symbol, 0);
+                }
+
+                symbols[symbol]++;
+            }
+
+            foreach (var kvp in symbols)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
