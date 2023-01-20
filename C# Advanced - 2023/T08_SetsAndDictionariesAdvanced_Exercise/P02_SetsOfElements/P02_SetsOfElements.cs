aea438337aba12d7cd76bc0edf09c719ed6f8d21
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 19:55:31 +0200

    T08-P02

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P02_SetsOfElements/P02_SetsOfElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secoundSet = new HashSet<int>();
+
+            int firstSetLenght = counts[0];
+            int secoundSetLenght = counts[1];
+
+            for (int i = 0; i < firstSetLenght + secoundSetLenght; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i < firstSetLenght)
+                {
+                    firstSet.Add(num);
+                }
+                else
+                {
+                    secoundSet.Add(num);
+                }
+            }
+
+            firstSet.IntersectWith(secoundSet);
+
+            Console.Write(string.Join(" ", firstSet));
+
+
+        }
+    }
+}
