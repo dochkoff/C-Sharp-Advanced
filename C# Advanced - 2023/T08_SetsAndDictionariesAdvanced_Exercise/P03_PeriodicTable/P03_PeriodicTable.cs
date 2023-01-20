7c336ddc5cfa8b22640879c6d2b0bc92377d5f03
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 20:16:05 +0200

    T08-P03

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P03_PeriodicTable/P03_PeriodicTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfSets = int.Parse(Console.ReadLine());
+
+            SortedSet<string> finalSet = new SortedSet<string>();
+
+            for (int i = 0; i < numOfSets; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                finalSet.UnionWith(elements);
+            }
+
+            Console.WriteLine(string.Join(" ", finalSet));
+        }
+    }
+}
