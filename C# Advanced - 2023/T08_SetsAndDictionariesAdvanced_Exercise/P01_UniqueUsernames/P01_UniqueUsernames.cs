725b78b59c4d878e8edb0e976b4b69961787232d
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 19:55:22 +0200

    T08-P01

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P01_UniqueUsernames/P01_UniqueUsernames.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
