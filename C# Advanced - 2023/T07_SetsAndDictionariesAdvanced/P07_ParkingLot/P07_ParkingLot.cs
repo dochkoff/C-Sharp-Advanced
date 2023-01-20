817e3219b5f6c0ee100441b6a72f7489be6aedfa
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 13:34:26 +0200

    T07-P07

---
+++ C# Advanced - 2023/T07_SetsAndDictionariesAdvanced/P07_ParkingLot/P07_ParkingLot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07_ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carPaltes = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] data = command.Split(", ");
+                string direction = data[0];
+                string plateNum = data[1];
+
+                if (direction == "IN")
+                {
+                    carPaltes.Add(plateNum);
+                }
+                else if (direction == "OUT")
+                {
+                    carPaltes.Remove(plateNum);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (carPaltes.Count > 0)
+            {
+                foreach (var plateNum in carPaltes)
+                {
+                    Console.WriteLine(plateNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
