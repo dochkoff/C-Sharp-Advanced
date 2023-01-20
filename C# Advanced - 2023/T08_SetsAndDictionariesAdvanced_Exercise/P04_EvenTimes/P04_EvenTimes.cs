f0bb8dbcc4062931d120f6118b9a31e981427f1f
dochkoff <hounds.07cinder@icloud.com>
2023-01-20 20:35:28 +0200

    T08-P04

---
+++ C# Advanced - 2023/T08_SetsAndDictionariesAdvanced_Exercise/P04_EvenTimes/P04_EvenTimes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numSet = new Dictionary<int, int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numSet.ContainsKey(num))
+                {
+                    numSet.Add(num, 0);
+                }
+
+                numSet[num]++;
+            }
+
+            Console.WriteLine(numSet.Single(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
