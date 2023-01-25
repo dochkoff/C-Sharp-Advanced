4f0f9797376557ca30ac66e5187db6a0fab8cdcc
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-25 18:24:03 +0200

    T10-P06

--- C# Advanced - 2023/T10_StreamsFilesAndDirectories_Exercise/ZipAndExtract/ZipAndExtract .cs
+++ C# Advanced - 2023/T10_StreamsFilesAndDirectories_Exercise/ZipAndExtract/ZipAndExtract .cs
@@ -2,13 +2,15 @@
 {
     using System;
     using System.IO;
+    using System.IO.Compression;
+
     public class ZipAndExtract
     {
         static void Main()
         {
-            string inputFile = @"..\..\..\copyMe.png";
-            string zipArchiveFile = @"..\..\..\archive.zip";
-            string extractedFile = @"..\..\..\extracted.png";
+            string inputFile = @"../../../copyMe.png";
+            string zipArchiveFile = @"../../../archive.zip";
+            string extractedFile = @"../../../extracted.png";
 
             ZipFileToArchive(inputFile, zipArchiveFile);
 
@@ -18,12 +20,20 @@
 
         public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
         {
-            throw new NotImplementedException();
+            using ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+
+            string fileName = Path.GetFileName(inputFilePath);
+
+            archive.CreateEntryFromFile(inputFilePath, fileName);
         }
 
         public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
         {
-            throw new NotImplementedException();
+            using ZipArchive archive = ZipFile.OpenRead(zipArchiveFilePath);
+
+            ZipArchiveEntry archiveEntry = archive.GetEntry(fileName);
+
+            archiveEntry.ExtractToFile(outputFilePath);
         }
     }
 }
