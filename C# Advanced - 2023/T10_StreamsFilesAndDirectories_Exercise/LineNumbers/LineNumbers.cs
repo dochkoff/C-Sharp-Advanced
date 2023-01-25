9a9c0cdd53d9890190615e1c4a20274cf3bf0a12
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-25 13:55:35 +0200

    T10-P02

--- C# Advanced - 2023/T10_StreamsFilesAndDirectories_Exercise/LineNumbers/LineNumbers.cs
+++ C# Advanced - 2023/T10_StreamsFilesAndDirectories_Exercise/LineNumbers/LineNumbers.cs
@@ -1,19 +1,34 @@
 ﻿namespace LineNumbers
 {
-    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
     public class LineNumbers
     {
         static void Main()
         {
-            string inputFilePath = @"..\..\..\text.txt";
-            string outputFilePath = @"..\..\..\output.txt";
+            string inputFilePath = @"../../../text.txt";
+            string outputFilePath = @"../../../output.txt";
 
             ProcessLines(inputFilePath, outputFilePath);
         }
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int lettersCount = lines[i].Count(char.IsLetter);
+                int punctuationCount = lines[i].Count(char.IsPunctuation);
+
+                sb.AppendLine($"Line {i + 1}: {lines[i]} ({lettersCount}({punctuationCount})");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
         }
     }
 }
