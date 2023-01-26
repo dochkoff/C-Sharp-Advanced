b9dd04908dfe51c30b956444228e37a1c281cd3e
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 18:39:30 +0200

    T12-P02

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P02_KnightsOfHonor/P02_KnightsOfHonor.cs
@@ -0,0 +1,12 @@
+﻿string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string[]> printNames = names =>
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Sir {name}");
+    }
+};
+
+printNames(names);
