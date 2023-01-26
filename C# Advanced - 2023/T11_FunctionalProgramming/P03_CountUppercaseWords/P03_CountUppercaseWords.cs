5b4d0d41d5466e19ba48c9966dda250fec9eb331
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 14:35:44 +0200

    T11-P03

---
+++ C# Advanced - 2023/T11_FunctionalProgramming/P03_CountUppercaseWords/P03_CountUppercaseWords.cs
@@ -0,0 +1,11 @@
+﻿Predicate<string> filterbyUppercase = s => s[0] == char.ToUpper(s[0]);
+
+string[] words = Console.ReadLine()
+    .Split()
+    .Where(s => filterbyUppercase(s))
+    .ToArray();
+
+foreach (var item in words)
+{
+    Console.WriteLine(item);
+}
