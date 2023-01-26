0acfaef5c1042fa2c23fcb0c9063205e79984495
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 18:31:13 +0200

    T12-P01

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P01_ActionPoint/P01_ActionPoint.cs
@@ -0,0 +1,6 @@
+﻿string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string[]> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+printNames(names);
