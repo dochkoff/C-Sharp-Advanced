7eeae8373918c7e92ebb57137ae8c1b7158be4fe
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 14:46:34 +0200

    Update P04_AddVAT.cs

--- C# Advanced - 2023/T11_FunctionalProgramming/P04_AddVAT/P04_AddVAT.cs
+++ C# Advanced - 2023/T11_FunctionalProgramming/P04_AddVAT/P04_AddVAT.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] nums = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .Select(x => x * 1.2)
+    .ToArray();
 
+foreach (var item in nums)
+{
+    Console.WriteLine($"{item:F2}");
+}
