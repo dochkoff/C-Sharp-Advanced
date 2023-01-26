8d33b23bb86e95391dfeaeed2900158ae9882fa3
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 22:33:47 +0200

    T12-P06

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P06_ReverseAndExclude/P06_ReverseAndExclude.cs
@@ -0,0 +1,38 @@
+﻿Func<List<int>, List<int>> reverce = numbers =>
+{
+    List<int> result = new();
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        result.Add(numbers[i]);
+    }
+
+    return result;
+};
+
+Func<List<int>, Predicate<int>, List<int>> excludeDevisible = (numbers, match) =>
+{
+    List<int> result = new();
+
+    foreach (var number in numbers)
+    {
+        if (!match(number))
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int devider = int.Parse(Console.ReadLine());
+
+numbers = excludeDevisible(numbers, n => n % devider == 0);
+numbers = reverce(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
