8fe5f22ef10bf18d18f1378be4d20219e9b70f36
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 18:54:07 +0200

    T12-P03

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P03_CustomMinFunction/P03_CustomMinFunction.cs
@@ -0,0 +1,20 @@
+﻿Func<HashSet<int>, int> min = numbers =>
+{
+    int min = int.MaxValue;
+
+    foreach (var num in numbers)
+    {
+        if (num < min)
+        {
+            min = num;
+        }
+    }
+    return min;
+};
+
+HashSet<int> nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+Console.WriteLine(min(nums));
