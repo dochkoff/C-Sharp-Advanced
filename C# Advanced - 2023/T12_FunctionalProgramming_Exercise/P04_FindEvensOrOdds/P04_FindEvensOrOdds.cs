898c8680063054388e6498d48aca0e6f3fdd46e2
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 19:22:15 +0200

    T12-P04

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P04_FindEvensOrOdds/P04_FindEvensOrOdds.cs
@@ -0,0 +1,39 @@
+﻿Func<int, int, List<int>> generateRange = (start, end) =>
+{
+    List<int> range = new();
+
+    for (int i = start; i <= end; i++)
+    {
+        range.Add(i);
+    }
+
+    return range;
+};
+
+Predicate<int> match;
+
+int[] range = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string command = Console.ReadLine();
+
+List<int> numbers = generateRange(range[0], range[1]);
+
+if (command == "even")
+{
+    match = numbers => numbers % 2 == 0;
+}
+else
+{
+    match = numbers => numbers % 2 != 0;
+}
+
+foreach (var number in numbers)
+{
+    if (match(number))
+    {
+        Console.Write(number + " ");
+    }
+}
