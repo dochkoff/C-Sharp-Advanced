e0ed1e8544c2b288e75225c1b023ff691bc4e7f9
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-26 22:10:45 +0200

    T12-P11

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P11_TriFunction/P11_TriFunction.cs
@@ -0,0 +1,12 @@
+﻿Func<string, int, bool> checkEqualOrLargerNameSum =
+    (name, sum) => name.Sum(ch => ch) >= sum;
+
+int sum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string[], int, Func<string, int, bool>, string> getFirstName =
+    (name, sum, match) => name.FirstOrDefault(name => match(name, sum));
+
+Console.WriteLine(getFirstName(names, sum, checkEqualOrLargerNameSum));
