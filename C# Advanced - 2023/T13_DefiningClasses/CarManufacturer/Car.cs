9d60b29794094d47c01c0af4ce136291984d9d25
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-28 14:09:34 +0200

    T13-P03

--- C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/Car.cs
+++ C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/Car.cs
@@ -3,6 +3,7 @@ namespace CarManufacturer
 {
     public class Car
     {
+        //fields
         private string make;
         private string model;
         private int year;
@@ -10,12 +11,40 @@ namespace CarManufacturer
         private double fuelConsumption;
         private string print;
 
+        //constructors
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+        : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        //properties
         public string Make { get { return make; } set { make = value; } }
         public string Model { get { return model; } set { model = value; } }
         public int Year { get { return year; } set { year = value; } }
         public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
         public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
 
+
+        //methods
         public void Drive(double distance)
         {
             if (fuelQuantity - (distance * fuelConsumption) > 0)
@@ -36,4 +65,3 @@ namespace CarManufacturer
         }
     }
 }
-
