c0a07c46a97478f5b2b04b5010b12e1bececefdc
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-28 18:36:26 +0200

    T13-P04

--- C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/Car.cs
+++ C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/Car.cs
@@ -10,6 +10,8 @@ namespace CarManufacturer
         private double fuelQuantity;
         private double fuelConsumption;
         private string print;
+        private Engine engine;
+        private Tire[] tires;
 
         //constructors
 
@@ -36,12 +38,20 @@ namespace CarManufacturer
             this.FuelConsumption = fuelConsumption;
         }
 
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
         //properties
         public string Make { get { return make; } set { make = value; } }
         public string Model { get { return model; } set { model = value; } }
         public int Year { get { return year; } set { year = value; } }
         public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
         public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
+        public Engine Engine { get { return engine; } set { engine = value; } }
+        public Tire[] Tires { get { return tires; } set { tires = value; } }
 
 
         //methods
--- C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/CarManufacturer.cs
+++ C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/CarManufacturer.cs
@@ -14,6 +14,18 @@ namespace CarManufacturer
             car.FuelQuantity = 200;
             car.FuelConsumption = 10;
 
+            Engine engin = new Engine(150, 2200);
+
+            car.Engine = engin;
+
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(1,2.5),
+                new Tire(1,2.5),
+                new Tire(2,2.2),
+                new Tire(2,2.2),
+            };
+
             car.Drive(10);
             Console.WriteLine(car.WhoAmI());
         }
