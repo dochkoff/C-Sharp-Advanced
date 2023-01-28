dec192fda975a458e73abc0c85c127814aa66829
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-28 10:54:48 +0200

    T13-P01

---
+++ C# Advanced - 2023/T13_DefiningClasses/CarManufacturer/CarManufacturer.cs
@@ -0,0 +1,18 @@
+﻿using CarManufacturer;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "Toyota";
+            car.Model = "Avensis";
+            car.Year = 2006;
+
+            Console.WriteLine($"My car is {car.Make} {car.Model} from {car.Year} year.");
+        }
+    }
+}
