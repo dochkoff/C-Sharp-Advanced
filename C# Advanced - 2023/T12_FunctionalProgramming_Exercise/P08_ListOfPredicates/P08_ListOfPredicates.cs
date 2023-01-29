993877e265e8946004cac9331100d4e3148b7aed
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-29 23:16:29 +0200

    T12-P08

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P08_ListOfPredicates/P08_ListOfPredicates.cs
@@ -0,0 +1,34 @@
+﻿List<Predicate<int>> predicates = new();
+
+int endOfRange = int.Parse(Console.ReadLine());
+
+HashSet<int> deviders = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+int[] numbers = Enumerable.Range(1, endOfRange).ToArray();
+
+foreach (var devider in deviders)
+{
+    predicates.Add(n => n % devider == 0);
+}
+
+foreach (var number in numbers)
+{
+    bool isDevisible = true;
+
+    foreach (var match in predicates)
+    {
+        if (!match(number))
+        {
+            isDevisible = false;
+            break;
+        }
+    }
+
+    if (isDevisible)
+    {
+        Console.Write($"{number} ");
+    }
+}
