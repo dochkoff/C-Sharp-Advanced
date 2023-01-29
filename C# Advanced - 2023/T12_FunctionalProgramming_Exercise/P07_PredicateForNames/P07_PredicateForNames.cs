6c2d627b5e5ea6d59676a089d66057cf91ac1bc8
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-29 19:42:40 +0200

    T12-P07

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P07_PredicateForNames/P07_PredicateForNames.cs
@@ -0,0 +1,13 @@
+﻿int nameLenght = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Predicate<string> filter = name => name.Length <= nameLenght;
+
+foreach (var name in names)
+{
+    if (filter(name))
+    {
+        Console.WriteLine(name);
+    }
+}
