a3f283d85d7b81b5c6ed85f2e071fff163d1cbf6
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-30 22:22:31 +0200

    T12-P10

---
+++ C# Advanced - 2023/T12_FunctionalProgramming_Exercise/P10_PartyReservationFilterModule/P10_PartyReservationFilterModule.cs
@@ -0,0 +1,51 @@
+﻿List<string> people = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Dictionary<string, Predicate<string>> filters = new();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Print")
+{
+    string[] tokens = command.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+    string action = tokens[0];
+    string filterType = tokens[1];
+    string filterParam = tokens[2];
+
+    if (action == "Add filter")
+    {
+        filters.Add(filterType + filterParam, GetPredicate(filterType, filterParam));
+    }
+    else
+    {
+        filters.Remove(filterType + filterParam);
+    }
+
+}
+
+foreach (var filter in filters)
+{
+    people.RemoveAll(filter.Value);
+}
+
+Console.WriteLine($"{string.Join(" ", people)}");
+
+
+static Predicate<string> GetPredicate(string filterType, string filterParam)
+{
+    switch (filterType)
+    {
+        case "Starts with":
+            return p => p.StartsWith(filterParam);
+        case "Ends with":
+            return p => p.EndsWith(filterParam);
+        case "Contains":
+            return p => p.Contains(filterParam);
+        case "Length":
+            return p => p.Length == int.Parse(filterParam);
+        default:
+            return default;
+    }
+}
