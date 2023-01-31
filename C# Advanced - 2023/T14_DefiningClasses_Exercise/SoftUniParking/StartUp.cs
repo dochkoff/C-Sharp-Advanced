477229127ca9d39daf9e91047e9fb2f4486733c0
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-31 18:04:47 +0200

    T14-P01

---
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/SoftUniParking/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person peter = new();
+
+            peter.Name = "Peter";
+            peter.Age = 20;
+        }
+    }
+}
