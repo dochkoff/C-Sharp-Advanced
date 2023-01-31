8d3bd131d15f4fa4d26f5c6ef2f688a90e78f410
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-31 23:55:04 +0200

    T14-P05

---
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/P05_DateModifier/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static void GetDaysBetweenToDates(string start, string end)
+        {
+            DateTime startDate = DateTime.Parse(start);
+            DateTime endDate = DateTime.Parse(end);
+
+            var difference = endDate - startDate;
+
+            Console.WriteLine(Math.Abs(difference.Days));
+        }
+    }
+}
---
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/P05_DateModifier/P05_DateModifier.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class P05_DateModifier
+    {
+        public static void Main(string[] args)
+        {
+            string startData = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier.GetDaysBetweenToDates(startData, endDate);
+        }
+    }
+}
