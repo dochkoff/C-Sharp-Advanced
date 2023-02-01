0fabd33188444aa47ceb0d138e832133c4d3a761
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-01 19:14:24 +0200

    T14-P06

---
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/P06_SpeedRacing/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace P06_SpeedRacing
+{
+    public class Car
+    {
+        public Car()
+        {
+            TravelledDistance = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+            : this()
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double amountOfKm)
+        {
+            if (amountOfKm * FuelConsumptionPerKilometer <= FuelAmount)
+            {
+                FuelAmount -= amountOfKm * FuelConsumptionPerKilometer;
+                TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
+
---
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/P06_SpeedRacing/P06_SpeedRacing.cs
@@ -0,0 +1,44 @@
+﻿using P06_SpeedRacing;
+
+int count = int.Parse(Console.ReadLine());
+
+HashSet<Car> cars = new();
+
+for (int i = 0; i < count; i++)
+{
+    string[] carProps = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Car car = new(carProps[0], double.Parse(carProps[1]), double.Parse(carProps[2]));
+
+    cars.Add(car);
+}
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "End")
+    {
+        break;
+    }
+
+    string[] cmdArgs = command
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string model = cmdArgs[1];
+    double distance = double.Parse(cmdArgs[2]);
+
+    foreach (var car in cars)
+    {
+        if (car.Model == model)
+        {
+            car.Drive(distance);
+        }
+    }
+}
+
+foreach (var car in cars)
+{
+    Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+}
