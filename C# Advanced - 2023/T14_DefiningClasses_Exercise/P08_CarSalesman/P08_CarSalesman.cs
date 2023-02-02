04ab903fa80920e1483db37603c840bdffe2f3de
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-02 22:16:18 +0200

    Update P08_CarSalesman.cs

--- C# Advanced - 2023/T14_DefiningClasses_Exercise/P08_CarSalesman/P08_CarSalesman.cs
+++ C# Advanced - 2023/T14_DefiningClasses_Exercise/P08_CarSalesman/P08_CarSalesman.cs
@@ -10,31 +10,7 @@ for (int i = 0; i < numOfEngines; i++)
     string[] engineProps = Console.ReadLine()
         .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-    string model = engineProps[0];
-    int power = int.Parse(engineProps[1]);
-
-    Engine engine = new(model, power);
-
-    if (engineProps.Length == 3)
-    {
-        if (int.TryParse(engineProps[2], out int displacement))
-        {
-            engine.Displacement = displacement;
-        }
-        else
-        {
-            string efficiency = engineProps[2];
-            engine.Efficiency = efficiency;
-        }
-    }
-    else if (engineProps.Length == 4)
-    {
-        int displacement = int.Parse(engineProps[2]);
-        string efficiency = engineProps[3];
-
-        engine.Displacement = displacement;
-        engine.Efficiency = efficiency;
-    }
+    Engine engine = EngineCreate(engineProps);
 
     engines.Add(engine);
 }
@@ -46,40 +22,7 @@ for (int i = 0; i < numOfCars; i++)
     string[] carProps = Console.ReadLine()
         .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-    string model = carProps[0];
-
-    Engine carEngine = new();
-    foreach (var engin in engines)
-    {
-        if (engin.Model == carProps[1])
-        {
-            carEngine = engin;
-        }
-    }
-
-    Car car = new(model, carEngine);
-
-    if (carProps.Length == 3)
-    {
-        if (int.TryParse(carProps[2], out int weight))
-        {
-            car.Weight = weight;
-        }
-        else
-        {
-            car.Color = carProps[2];
-        }
-
-
-    }
-    else if (carProps.Length == 4)
-    {
-        int weight = int.Parse(carProps[2]);
-        string color = carProps[3];
-
-        car.Weight = weight;
-        car.Color = color;
-    }
+    Car car = CarCreate(engines, carProps);
 
     cars.Add(car);
 }
@@ -87,4 +30,59 @@ for (int i = 0; i < numOfCars; i++)
 foreach (var car in cars)
 {
     Console.WriteLine(car);
+}
+
+static Engine EngineCreate(string[] engineProps)
+{
+    string model = engineProps[0];
+    int power = int.Parse(engineProps[1]);
+
+    Engine engine = new(model, power);
+
+    if (engineProps.Length > 2)
+    {
+        if (int.TryParse(engineProps[2], out int displacement))
+        {
+            engine.Displacement = int.Parse(engineProps[2]);
+        }
+        else
+        {
+            engine.Efficiency = engineProps[2];
+        }
+
+        if (engineProps.Length > 3)
+        {
+            engine.Efficiency = engineProps[3];
+        }
+    }
+
+    return engine;
+}
+
+static Car CarCreate(List<Engine> engines, string[] carProps)
+{
+    string model = carProps[0];
+
+    Engine carEngine = engines.Find(e => e.Model == carProps[1]);
+
+    Car car = new(model, carEngine);
+
+    if (carProps.Length > 2)
+    {
+        if (int.TryParse(carProps[2], out int weight))
+        {
+            car.Weight = int.Parse(carProps[2]);
+        }
+        else
+        {
+            car.Color = carProps[2];
+        }
+
+        if (carProps.Length > 3)
+        {
+            car.Color = carProps[3];
+        }
+    }
+
+    return car;
 }
