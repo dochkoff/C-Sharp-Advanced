e7b109591dae5087df3731e35c346c8177ac3a06
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-06 12:56:52 +0200

    T16-P03

--- C# Advanced - 2023/T16_ImplementingStackAndQueue/P03_CustomQueue/CustomQueue.cs
+++ C# Advanced - 2023/T16_ImplementingStackAndQueue/P03_CustomQueue/CustomQueue.cs
@@ -3,8 +3,98 @@ namespace P03_CustomQueue
 {
     public class CustomQueue
     {
+        private const int IniitialCapacity = 4;
+        private const int FirstItemIndex = 0;
+        private int[] items;
+        private int count;
+
         public CustomQueue()
         {
+            count = 0;
+            items = new int[IniitialCapacity];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public void Enqueue(int item)
+        {
+            if (items.Length == count)
+            {
+                Resize();
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        public int Dequeue()
+        {
+            IsEmpty();
+            int fisrtItem = items[FirstItemIndex];
+            SwitchElements();
+            count--;
+
+            return fisrtItem;
+        }
+
+        public int Peek()
+        {
+            IsEmpty();
+
+            int firstItem = items[FirstItemIndex];
+            return firstItem;
+        }
+
+        public void Clear()
+        {
+            IsEmpty();
+            items = new int[IniitialCapacity];
+            count = 0;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        private void IsEmpty()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("CustomQueue is empty");
+            }
+        }
+
+        private void SwitchElements()
+        {
+            items[FirstItemIndex] = default;
+
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                items[i - 1] = items[i];
+            }
         }
     }
 }
--- C# Advanced - 2023/T16_ImplementingStackAndQueue/P03_CustomQueue/Program.cs
+++ C# Advanced - 2023/T16_ImplementingStackAndQueue/P03_CustomQueue/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using P03_CustomQueue;
 
+CustomQueue customQueue = new();
+
+customQueue.Enqueue(10);
+customQueue.Enqueue(11);
+customQueue.Enqueue(12);
+customQueue.Enqueue(13);
+customQueue.Enqueue(14);
+
+customQueue.ForEach(i => Console.Write(i + " "));
+Console.WriteLine();
+
+Console.WriteLine(customQueue.Dequeue());
+
+Console.WriteLine(customQueue.Peek());
+
+customQueue.Clear();
+Console.WriteLine(customQueue.Count);
