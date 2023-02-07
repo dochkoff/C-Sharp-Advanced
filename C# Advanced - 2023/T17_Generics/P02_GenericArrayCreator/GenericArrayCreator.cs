67aa002d78ed1551bdc1debbcf544a095758329a
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-07 21:58:24 +0200

    T17-P02

---
+++ C# Advanced - 2023/T17_Generics/P02_GenericArrayCreator/GenericArrayCreator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = ArrayCreator.Create(5, 5);
+        }
+    }
+}
