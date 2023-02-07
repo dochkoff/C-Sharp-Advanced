6b1213ff8b6373a4bdcc7e9ce60caee1e2e8914e
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-07 21:18:18 +0200

    T17-P01

---
+++ C# Advanced - 2023/T17_Generics/P01_BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private int count;
+        private List<T> list;
+
+        public Box()
+        {
+            count = 0;
+            list = new();
+        }
+
+        public int Count { get { return this.count; } }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+            count++;
+        }
+
+        public T Remove()
+        {
+            T removed = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            count--;
+            return removed;
+        }
+    }
+}
+
---
+++ C# Advanced - 2023/T17_Generics/P01_BoxOfT/BoxOfT.cs
@@ -0,0 +1,18 @@
+﻿using BoxOfT;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Box<int> box = new();
+
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
