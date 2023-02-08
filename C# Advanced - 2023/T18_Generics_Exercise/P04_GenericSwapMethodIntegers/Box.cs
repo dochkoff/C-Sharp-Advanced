1860c2ce1faefa4a45abc1796cab325520b0fa23
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 01:05:10 +0200

    T18-P04

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P04_GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            T temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced - 2023/T18_Generics_Exercise/P04_GenericSwapMethodIntegers/Startup.cs
@@ -0,0 +1,21 @@
+﻿using GenericSwapMethodIntegers;
+
+Box<int> box = new();
+
+int inputNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < inputNumber; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+int[] indexes = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+box.Swap(indexes[0], indexes[1]);
+
+Console.WriteLine(box.ToString());
