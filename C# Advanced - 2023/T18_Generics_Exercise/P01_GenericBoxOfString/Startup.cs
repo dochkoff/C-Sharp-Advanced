8d3c42c72346412db6e1f33f553a1533385e2ac1
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 00:03:18 +0200

    T18-P01

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P01_GenericBoxOfString/Startup.cs
@@ -0,0 +1,14 @@
+﻿using GenericBoxOfString;
+
+Box<string> box = new();
+
+int numberOfInputes = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfInputes; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+Console.WriteLine(box.ToString());
