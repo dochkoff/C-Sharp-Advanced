efe3666527f8641e779cb4b7e6d7e180d9c7a5d7
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 22:24:57 +0200

    T18-P09

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P09_CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        private class ListNode
+        {
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+        }
+        private ListNode head;
+        private ListNode tail;
+
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidProgramException("The list is empty");
+            }
+
+            T firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidProgramException("The list is empty");
+            }
+
+            T lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            ListNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+            int counter = 0;
+            ListNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+            return array;
+        }
+    }
+}
+
---
+++ C# Advanced - 2023/T18_Generics_Exercise/P09_CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            DoublyLinkedList<string> list = new();
+
+            list.AddFirst("a");
+            list.AddFirst("b");
+            list.AddFirst("c");
+            list.AddFirst("d");
+            list.AddFirst("e");
+            list.AddFirst("f");
+
+            list.AddLast("z");
+
+            list.RemoveFirst();
+
+            list.RemoveLast();
+
+            Action<string> action = new Action<string>(x => Console.WriteLine(x));
+            list.ForEach(action);
+
+            string[] arr = list.ToArray();
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
