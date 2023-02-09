928fee9ba5fd14c6ce15287f4e10dfdce8210076
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 11:54:50 +0200

    T18-P06

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P06_GenericCountMethodDoubles/Startup.cs
@@ -0,0 +1,15 @@
+﻿using GenericCountMethodDoubles;
+
+int numOfItems = int.Parse(Console.ReadLine());
+
+Box<double> box = new();
+
+for (int i = 0; i < numOfItems; i++)
+{
+    double item = double.Parse(Console.ReadLine());
+    box.Add(item);
+}
+
+double element = double.Parse(Console.ReadLine());
+
+Console.WriteLine(box.Count(element));
