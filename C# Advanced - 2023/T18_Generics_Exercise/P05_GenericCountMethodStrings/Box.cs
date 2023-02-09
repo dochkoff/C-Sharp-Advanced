bb6e3c4deb163193e062d2e2f604dd603d3bec5d
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 11:54:42 +0200

    T18-P05

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P05_GenericCountMethodStrings/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public int Count(T value)
+        {
+            int count = 0;
+
+            foreach (var item in items)
+            {
+                if (item.CompareTo(value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# Advanced - 2023/T18_Generics_Exercise/P05_GenericCountMethodStrings/Startup.cs
@@ -0,0 +1,15 @@
+﻿using GenericCountMethodStrings;
+
+int numOfItems = int.Parse(Console.ReadLine());
+
+Box<string> box = new();
+
+for (int i = 0; i < numOfItems; i++)
+{
+    string item = Console.ReadLine();
+    box.Add(item);
+}
+
+string element = Console.ReadLine();
+
+Console.WriteLine(box.Count(element));
