4a6a1775aaa1c65059c7664a1b8d2db797c242c2
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 13:29:07 +0200

    T18-P08

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P08_Threeuple/Startup.cs
@@ -0,0 +1,26 @@
+﻿using CustomThreeuple;
+
+string[] personInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] drunkInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] bankInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+Threeuple<string, string, string> person =
+    new($"{personInfo[0]} {personInfo[1]}", personInfo[2], string.Join(" ", personInfo[3..]));
+
+Threeuple<string, int, bool> isDrunk =
+    new(drunkInfo[0], int.Parse(drunkInfo[1]), drunkInfo[2] == "drunk");
+
+Threeuple<string, double, string> accBanalce =
+    new(bankInfo[0], double.Parse(bankInfo[1]), bankInfo[2]);
+
+Console.WriteLine(person);
+Console.WriteLine(isDrunk);
+Console.WriteLine(accBanalce);
