f3c3c618f42d6c91786abcd2bbb24e48cd36ac2f
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-09 13:12:02 +0200

    T18-P07

---
+++ C# Advanced - 2023/T18_Generics_Exercise/P07_Tuple/Startup.cs
@@ -0,0 +1,27 @@
+﻿
+using Tuple;
+
+string[] personInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] drinkBeer = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+CustomTuple<string, string> personTuple =
+    new($"{personInfo[0]} {personInfo[1]}", personInfo[2]);
+
+CustomTuple<string, int> beerTuple =
+    new(drinkBeer[0], int.Parse(drinkBeer[1]));
+
+CustomTuple<int, double> numbersTuple =
+    new(int.Parse(numbers[0]), double.Parse(numbers[1]));
+
+Console.WriteLine(personTuple);
+Console.WriteLine(beerTuple);
+Console.WriteLine(numbersTuple.ToString());
