075a1aa8d542a5740d6df22b1aa01e1a9a4dcb3f
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-11 00:36:56 +0200

    T20-P03

---
+++ C# Advanced - 2023/T20_IteratorsAndComparators_Exercise/CustomStack/CustomStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private int initialSize = 4;
+        T[] items;
+
+        public CustomStack()
+        {
+            this.items = new T[initialSize];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            this.items[Count] = element;
+
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T removedItem = items[Count - 1];
+            Count--;
+            return removedItem;
+        }
+
+        private T[] Resize()
+        {
+            T[] copy = new T[items.Length * 2];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+
+            return items;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
+
---
+++ C# Advanced - 2023/T20_IteratorsAndComparators_Exercise/CustomStack/Program.cs
@@ -0,0 +1,45 @@
+﻿using Stack;
+
+CustomStack<int> stack = new();
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] items = command
+    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+    string action = items[0];
+
+    if (action == "Push")
+    {
+        int[] elementsToPush = items
+            .Skip(1)
+            .Select(int.Parse)
+            .ToArray();
+
+        foreach (var item in elementsToPush)
+        {
+            stack.Push(item);
+        }
+    }
+    else if (action == "Pop")
+    {
+        try
+        {
+            stack.Pop();
+        }
+        catch (InvalidOperationException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
