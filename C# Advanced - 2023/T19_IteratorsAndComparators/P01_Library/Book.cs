2d3226396e5436e46df95fca768161f40a1d818b
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-10 14:49:33 +0200

    T19-P01

---
+++ C# Advanced - 2023/T19_IteratorsAndComparators/P01_Library/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public IReadOnlyList<string> Authors { get; private set; }
+    }
+}
+
