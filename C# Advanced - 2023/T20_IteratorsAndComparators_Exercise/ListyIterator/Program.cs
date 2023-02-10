f4ef66079241443520f5d8ea1488e017fc1261f5
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-10 23:20:26 +0200

    T20-P01

---
+++ C# Advanced - 2023/T20_IteratorsAndComparators_Exercise/ListyIterator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using ListyIterator;
+
+List<string> items = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Skip(1)
+    .ToList();
+
+ListyIterator<string> collection = new(items);
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command == "Move")
+    {
+        Console.WriteLine(collection.Move());
+    }
+    else if (command == "HasNext")
+    {
+        Console.WriteLine(collection.HasNext());
+    }
+    else if (command == "Print")
+    {
+        collection.Print();
+    }
+    else
+    {
+        Console.WriteLine("Invalid Operation!");
+    }
+}
