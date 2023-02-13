bbc5fa29cd0db50776636964d3efe1b7e61532f7
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-13 22:58:31 +0200

    T20-P04

---
+++ C# Advanced - 2023/T20_IteratorsAndComparators_Exercise/P04_Froggy/Program.cs
@@ -0,0 +1,11 @@
+﻿using Froggy;
+
+
+List<int> stones = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+Lake lake = new Lake(stones);
+
+Console.WriteLine(string.Join(", ", lake));
