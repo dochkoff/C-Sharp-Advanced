a4aedd95a5740c0c3748ed27af8f71708af9d6f6
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-16 21:24:41 +0200

    T21-P03

---
+++ C# Advanced - 2023/T21_ExamPreparation/P03_ComputerArchitecture/CPU.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        public CPU(string brand, int cores, double frequency)
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public string Brand { get; set; }
+        public int Cores { get; set; }
+        public double Frequency { get; set; }
+
+        public override string ToString()
+        {
+            return ($"{this.Brand} CPU:\nCores: {Cores}\nFrequency: {Frequency:F1} GHz");
+        }
+    }
+}
---
+++ C# Advanced - 2023/T21_ExamPreparation/P03_ComputerArchitecture/Computer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public List<CPU> Multiprocessor { get; set; }
+
+        public int Count => Multiprocessor.Count;
+
+        public void Add(CPU processor)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(processor);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            bool containsBrand = false;
+            CPU removedCPU = null;
+
+            foreach (var processor in Multiprocessor)
+            {
+                if (processor.Brand == brand)
+                {
+                    containsBrand = true;
+                    removedCPU = processor;
+                }
+            }
+
+            if (containsBrand == true)
+            {
+                Multiprocessor.Remove(removedCPU);
+            }
+
+            return containsBrand;
+        }
+
+        public CPU MostPowerful()
+        {
+            CPU peekCPU = Multiprocessor[0];
+
+            foreach (var processor in Multiprocessor)
+            {
+                if (processor.Frequency > peekCPU.Frequency)
+                {
+                    peekCPU = processor;
+                }
+            }
+
+            return peekCPU;
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            foreach (var processor in Multiprocessor)
+            {
+                if (processor.Brand == brand)
+                {
+                    return processor;
+                }
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            string report = $"CPUs in the Computer {this.Model}:";
+
+            foreach (var processor in Multiprocessor)
+            {
+                report += processor.ToString() + "\n";
+            }
+
+            return report.TrimEnd();
+        }
+    }
+}
