67ba24ab63a41ad89c9001304dd5f21265b636fd
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-18 19:46:40 +0200

    T24-P03

---
+++ C# Advanced - 2023/T24_RegularExam/P03/Kindergarten.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public int ChildrenCount => Registry.Count;
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            string[] names = childFullName
+                .Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+            string currenChildFirstName = names[0];
+            string currenChildLastName = names[1];
+
+            foreach (var child in Registry)
+            {
+                if (child.FirstName == currenChildFirstName && child.LastName == currenChildLastName)
+                {
+                    Registry.Remove(child);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            string[] names = childFullName
+                .Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+            string currenChildFirstName = names[0];
+            string currenChildLastName = names[1];
+
+            foreach (var child in Registry)
+            {
+                if (child.FirstName == currenChildFirstName && child.LastName == currenChildLastName)
+                {
+                    Registry.Remove(child);
+                    return child;
+                }
+            }
+            return null;
+        }
+
+        public string RegistryReport()
+        {
+
+
+            StringBuilder sb = new();
+
+            sb.AppendLine($"Registered children in {Name}:");
+
+            foreach (var child in Registry.OrderByDescending(c => c.Age).ThenBy(c => c.LastName).ThenBy(c => c.FirstName))
+            {
+                sb.AppendLine(child.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+
+
+        }
+    }
+}
