594c8c2cc92eb3fc413844ddccc46918025920f9
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-23 00:37:18 +0200

    T23-P05

---
+++ C# Advanced - 2023/T23_AlgorithmsIntroduction/P05_MergeSort/P05_MergeSort.cs
@@ -0,0 +1,67 @@
+﻿namespace MergeSort
+{
+    class Program
+    {
+        private static int[] supp;
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            supp = new int[nums.Length];
+            MergeSort(nums, 0, nums.Length - 1);
+
+            Console.WriteLine(string.Join(" ", nums));
+
+        }
+
+        static void Merge(int[] input, int left, int middle, int right)
+        {
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            Array.Copy(input, left, leftArray, 0, middle - left + 1);
+            Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+            int i = 0;
+            int j = 0;
+            for (int k = left; k < right + 1; k++)
+            {
+                if (i == leftArray.Length)
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+            }
+        }
+
+        static void MergeSort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                MergeSort(input, left, middle);
+                MergeSort(input, middle + 1, right);
+
+                Merge(input, left, middle, right);
+            }
+        }
+    }
+}
