af1d8271ae575ee8aa534737d922f1ee8ff0b358
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-22 17:10:53 +0200

    T23-P02

---
+++ C# Advanced - 2023/T23_AlgorithmsIntroduction/P02_RecursiveFactorial/P02_RecursiveFactorial.cs
@@ -0,0 +1,13 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FactorialRecursion(number));
+
+static int FactorialRecursion(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+
+    return number * FactorialRecursion(number - 1);
+}
